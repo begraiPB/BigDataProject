75f5304458c2ae5c303d6db5ebfba275327cf7b7
begraiPB <parineeta.begrai@sjsu.edu>
2023-05-02 22:12:51 -0700

    Added authentication.

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "bigdataproject8635ff21": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  }
+}
--- src/App.js
+++ src/App.js
@@ -1,6 +1,24 @@
+import logo from "./logo.svg";
 import BarChart from './BarChart'
+import "@aws-amplify/ui-react/styles.css";
+import {
+  withAuthenticator,
+  Button,
+  Heading,
+  Image,
+  View,
+  Card,
+} from "@aws-amplify/ui-react";
 
-const App = () => {
-    return ( <BarChart /> )
+const App = ({ signOut, user }) => {
+    return (<div>
+                <View className="App">
+                    <Card>
+                    <Heading level={1}>Hello {user.username}! You are Authenticated!</Heading>
+                    </Card>
+                    <Button onClick={signOut}>Sign Out</Button>
+                </View>
+                <BarChart /> 
+            </div> )
 }
-export default App;
+export default withAuthenticator(App);
